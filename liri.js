619d85cd17a2e9a5d243b01eb2688a6de75342ff
Joshua Miller <jlmiller07@gmail.com>
2017-07-22 19:03:25 -0500

    app functioning fully with command logging

--- liri.js
+++ liri.js
@@ -6,25 +6,30 @@ var fs = require("fs");
 var keys = require("./keys.js");
 
 var command = process.argv[2];
+var txtLogCommand = "\rCommand: ";
+
+for (var i = 2; i < process.argv.length; i++) {
+    txtLogCommand += process.argv[i] + " "; 
+}
 
 //Based on command function is selected
 if (command === 'my-tweets'){
-    console.log("Run my tweets");
     getTweets();
+    logCommand(txtLogCommand);
     }
 else if (command === 'spotify-this-song'){
-    console.log("Run Spotify");
     getSpotify();
+    logCommand(txtLogCommand);
     }
 else if (command === 'movie-this'){
-    console.log("Run OMDB");
     getOMDB();
-    logCommand(command);
+    logCommand(txtLogCommand);
     }
 else if (command === 'do-what-it-says'){
-    console.log("Run do what it says");
     doWhatItSays();
+    logCommand(txtLogCommand);
     }
+
 else {console.log("I'm sorry. I don't recognize that command. Please try again.");}
 
 //my-tweets
@@ -41,38 +46,53 @@ function getTweets(){
     var params = {screen_name: 'cgs_robot'};
 
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    console.log("--------------------------");
+    console.log("+++++++TWITTER FEED+++++++");
+    console.log("--------------------------");
     if (!error) {
         for (var i = 0; i < 20; i++) {
-            console.log((i+1) + ". " + tweets[i].text);    
+            console.log((i+1) + ". " + tweets[i].text + " (Posted: " + tweets[i].created_at + ")");    
         }
     }
+    console.log("--------------------------");
     })
 }
 
 //spotify-this-song
-function getSpotify(){
-    var songName = process.argv[3];
+function getSpotify(songName){
 
-    if (songName === null) {
-        songName = "The Sign";
+    if (process.argv[3] !=null){
+        songName = process.argv[3];
+    }
+    else if (songName === undefined) {
+        songName = "ace sign";
     }
 
+var spotifyKeys = keys.spotifyKeys;    
  
 var spotify = new Spotify({
-  id: '15ff7a7cb240490794fa1a00cf847879',
-  secret: '018c8d910c6648a99e53788acb9e1612'
+  id: spotifyKeys.id,
+  secret: spotifyKeys.secret,
 });
  
 spotify
-  .search({ type: 'track', query: songName })
+  .search({ type: 'track', query: songName, limit: 3})
   .then(function(response) {
     console.log("--------------------------");
-    console.log("Artist Name: " + response.tracks.items[0].artists[0].name);
-    console.log("Song Name: " + response.tracks.items[0].name);
-    console.log("Album Name: " + response.tracks.items[0].album.name);
-    console.log("Preview Link: " + response.tracks.items[0].preview_url);
-    console.log("--------------------------");
-
+    console.log("++SPOTIFY SEARCH RESULTS++");
+    for (var i = 0; i < (response.tracks.items.length); i++) {
+        console.log("--------------------------");
+        for (var j = 0; j < response.tracks.items[i].artists.length; j++) {
+            if (j === 0){
+                var artistList = response.tracks.items[i].artists[j].name;
+            }
+            else {artistList += ", " + response.tracks.items[i].artists[j].name;}    
+        }
+        console.log("Artist(s): " + artistList);
+        console.log("Song Name: " + response.tracks.items[i].name);
+        console.log("Album Name: " + response.tracks.items[i].album.name);
+        console.log("Preview Link: " + response.tracks.items[i].preview_url);
+    }
   })
   .catch(function(err) {
     console.log(err);
@@ -80,15 +100,21 @@ spotify
 }
 //movie-this
 function getOMDB(){
-    var movieName = process.argv[3];
+    if (process.argv[3] === undefined) {
+        movieName = "Mr. Nobody";
+        }
+    else {var movieName = process.argv[3];}
+
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
 
-   // if (movieName === null) {
-     //   movieName = "Mr. Nobody";
-    //}
+    if (movieName === "") {
+    movieName = "Mr. Nobody";
+    } 
 
     request(queryUrl, function(error, response, body){
         if (!error && response.statusCode === 200) {
+        console.log("--------------------------");
+        console.log("+++MOVIE SEARCH RESULTS+++");
         console.log("--------------------------");
         console.log("Title: " + JSON.parse(body).Title);
         console.log("Year: " + JSON.parse(body).Year);
@@ -113,17 +139,15 @@ function doWhatItSays(){
     var commandInternal = dataArr[0];
     var songName = dataArr[1];
 
-    console.log(commandInternal);
-    console.log(songName);
-    
-    /*for (var i = 0; i < dataArr.length; i++) {
-        //var string = dataArr[i];
-        console.log(dataArr[i].trim());        
-        } */
+    getSpotify(songName);
     
     })
 }
 
-function logCommand(command){
-    fs.appendFile("log.txt", "utf8", command);
-}
+function logCommand(txtLogCommand){
+    fs.appendFile("log.txt", txtLogCommand, function(err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+};
